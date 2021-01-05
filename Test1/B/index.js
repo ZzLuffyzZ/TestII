ef4a5a66b543f3d734a0073988b97dff4b5a1c50
ZzLuffyzZ <70495382+ZzLuffyzZ@users.noreply.github.com>
2021-01-05 17:46:31 +0700

    Update index.js

--- Test1/B/index.js
+++ Test1/B/index.js
@@ -1,37 +1,41 @@
+
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
 const myQuestions = [{"category":"Sports","type":"multiple","difficulty":"easy",
                     question:"In golf, what name is given to a hole score of two under par?",
-                    correct_answer:"3",answers:["Birdie","Bogey","Albatross","Eagle"]},
+                    correct_answer:"Eagle",incorrect_answers:["Birdie","Bogey","Albatross"]},
                     {"category":"Sports","type":"multiple","difficulty":"easy",
                     question:"Which team won 2014 FIFA World Cup in Brazil?",
-                    correct_answer:`3`,answers:["Argentina","Brazil","Netherlands","Germany"]},
+                    correct_answer:"Germany",incorrect_answers:["Argentina","Brazil","Netherlands"]},
                     {"category":"Sports","type":"multiple","difficulty":"easy",
                     question:"In Baseball, how many times does the ball have to be pitched outside of the strike zone before the batter is walked?",
-                    correct_answer:`3`,answers:["1","2","3","4"]},
+                    correct_answer:"4",incorrect_answers:["1","2","3"]},
                     {"category":"Sports","type":"multiple","difficulty":"easy",
                     question:"Who is often called &quot;the Maestro&quot; in the men&#039;s tennis circuit?",
-                    correct_answer:"3",answers:["Bill Tilden","Boris Becker","Pete Sampras","Roger Federer"]},
+                    correct_answer:"Roger Federer",incorrect_answers:["Bill Tilden","Boris Becker","Pete Sampras"]},
                     {"category":"Sports","type":"multiple","difficulty":"easy",
                     question:"Who won the UEFA Champions League in 2016?",
-                    correct_answer:"3",answers:["FC Bayern Munich","Atletico Madrid","Manchester City F.C.","Real Madrid C.F."]}]
+                    correct_answer:"Real Madrid C.F.",incorrect_answers:["FC Bayern Munich","Atletico Madrid","Manchester City F.C.",]}]
+  
 
-function buildQuiz() {
+  function buildQuiz() {
     const output = [];
   
     myQuestions.forEach(
       (currentQuestion, questionNumber) => {
   
         const answers = [];
-      for (letter in currentQuestion.answers) {
+      for (letter in currentQuestion.incorrect_answers) {
 
 
         answers.push(
           `<label>
+
+
                 <input type="radio" name="question${questionNumber}" value="${letter}">
-                ${currentQuestion.answers[letter]}
+                ${currentQuestion.incorrect_answers[letter]}
 
               </label>`
         );
@@ -41,7 +45,10 @@ function buildQuiz() {
             `<div class="question"> ${currentQuestion.question} </div>
                 <div class="answers"> 
                 ${answers.join('')}
-                </div>
+                <input type="radio" name="question${questionNumber}">
+                ${currentQuestion.correct_answer}
+
+                 </div>
                 `
           );
         //console.log(output);
@@ -64,7 +71,7 @@ function buildQuiz() {
       const userAnswer = [(answerContainer.querySelector(selector) || ``).value];
       console.log(userAnswer);
       for (let i = 0; i < userAnswer.length; i++) {
-        if (userAnswer[i] === myQuestions[i].correct_answer) {
+        if (userAnswer[i] == "on") {
           numCorrect++;
   
           answerContainers[questionNumber].style.color = 'blue';
@@ -72,7 +79,7 @@ function buildQuiz() {
         else {
           answerContainers[questionNumber].style.color = 'red';
         }
-        console.log(myQuestions[i].correct_answer);
+        break;
 
         //console.log(userAnswer);
       }
@@ -85,7 +92,7 @@ function buildQuiz() {
                               2: Germany ,
                               3: 4,
                               4: Roger Federer ,
-                              5:Real Madrid C.F`)
+                              5 :Real Madrid C.F`)
   }
   
   
